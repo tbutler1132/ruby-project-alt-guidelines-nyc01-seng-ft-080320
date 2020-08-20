78a32920a44ced966920554198ea7829d87c57a2
github username <github email address>
2020-08-20 12:52:21 -0400

    added display_album method to Collection

--- lib/app/models/collection.rb
+++ lib/app/models/collection.rb
@@ -2,4 +2,16 @@ class Collection < ActiveRecord::Base
     belongs_to :user
     belongs_to :album
 
+    def self.display_albums(user)
+        user_collections = Collection.all.filter do |collection|
+            collection.user == user
+        end 
+        user_albums = user_collections.map do |collection|
+            collection.album
+        end
+        user_albums.each do |album|
+            puts album.artist + " - " + album.title.italic
+        end
+     end
+
 end
