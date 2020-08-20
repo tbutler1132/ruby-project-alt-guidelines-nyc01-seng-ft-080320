4622834ea7352072a70ec02a8d2d3bebf3ef1055
D1yr <alexander.m.sahli@gmail.com>
2020-08-20 12:47:53 -0400

    added a multifunction to the cli with an escape so user doesnt have to add or delete from match- alex

--- lib/app/models/cli.rb
+++ lib/app/models/cli.rb
@@ -6,7 +6,7 @@ require_relative 'collection.rb'
 
 def run
     prompt = TTY::Prompt.new
-    pid = fork{ exec ‘afplay’, 'music/file.mp3' }      
+    #pid = fork{ exec ‘afplay’, 'music/file.mp3' }      
     user_name = prompt.ask("Please input your name:".white.on_light_black.bold)
     current_user = User.find_by(name: user_name)
 system "clear"
@@ -18,13 +18,13 @@ system "clear"
     else choice == "match"
             puts "Your match is #{current_user.match.name}. Your match rate is #{current_user.percent_in_common}%!"
     end
-    choice = prompt.select("Would you like to add or delete an album from your collection?", %w(add delete))
+    choice = prompt.multi_select("Would you like to add or delete an album from your collection?", %w(add delete display))
 system "clear"
     if choice == "delete"
         puts "Which album would you like to delete?"
         album_title = gets.chomp
         current_user.delete_album(album_title)
-    else choice == "add"
+    elsif choice == "add"
         puts "Please input the album title."
         album_title = gets.chomp
         if Album.album_in_database?(Album.find_by(title: album_title))     
@@ -37,6 +37,10 @@ system "clear"
             puts "Please input the album label"
             album_label = gets.chomp
             current_user.add_album(nil, album_title, album_artist, album_genre, album_label)
+        end
+    else choice == "display"
+        current_user.albums.each do |album|
+            puts album.artist + " - " + album.title
         end
     end               
     choice = prompt.yes?("Your albums have been updated. Would you like to view your current collection?")
